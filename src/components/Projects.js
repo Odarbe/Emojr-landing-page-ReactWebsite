b03ab1e932b46248242fe0aaf66efd79f4ccd13b
junjun <odarbejr19@gmail.com>
2021-09-23 13:50:25 +0800

    changes path location src img

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,36 +2,36 @@ const PROJECTS = [
     {
         title: "Home Builders Buyer's Guide",
         discription: 'Digital Magazine aspires to be a hightly effective flatform in the dissemination of information on the most advanced tools in construction and interior design.',
-        image: '/assets/images/hbbg-front.png',
+        image: 'https://emojr1.netlify.app/img/hbbg-front.png',
         siteLink: 'https://www.hbbg.com.ph/'
     },
     {
         title: 'HBBG-Digital Magazine',
         discription: "Home Builders Buyer's Guide (UI) UserInterface Back-End of registration and viewing Digital Magazine.",
-        image: '/assets/images/hbbg-backend.png',
+        image: 'https://emojr1.netlify.app/img/hbbg-backend.png',
         siteLink: ''
     },
     {
         title: 'E-COMMERCE WEBSITE HBBG FRONT-END',
         discription: "Home Builders Buyer's Guide",
-        image: '/assets/images/HBBGFRONT.png',
+        image: 'https://emojr1.netlify.app/img/HBBGFRONT.png',
         siteLink: ''
     },
     {
         title: 'E-COMMERCE WEBSITE HBBG BACK-END',
         discription: "Home Builders Buyer's Guide",
-        image: '/assets/images/HBBGBCKEND.png',
+        image: 'https://emojr1.netlify.app/img/HBBGBCKEND.png',
         siteLink: ''
     },{
         title: 'WEBSITE J-FASHION',
         discription: "For me style is expressing yourself through what you wear. Licensed from iStockPhoto. noun. Style is defined as a particular of doing or saying something, or refers to a unique form of clothing or way of arranging your appearance.",
-        image: '/assets/images/dtm-view.png',
+        image: 'https://emojr1.netlify.app/img/dtm-view.png',
         siteLink: 'https://jfashion.netlify.app/'
     },
     {
         title: 'FRONT-END WEBSITE RESUME',
         discription: "Personal Website Resume, short information about Junjun Odarbe. Created the website in 2019.",
-        image: '/assets/images/resume-dtm.png',
+        image: 'https://emojr1.netlify.app/img/resume-dtm.png',
         siteLink: 'https://ernestoodarbejr-resume.netlify.app/'
     }
 ]
